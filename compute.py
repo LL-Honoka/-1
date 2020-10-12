13ccfcb60cf674df866af5b28b85ab106f5879d7
Ubuntu <1023064692@qq.com>
2020-10-12 23:54:36 +0800

    Social-Computing_Project_One

--- compute.py
+++ compute.py
@@ -2,17 +2,28 @@ import jieba
 import csv
 
 input_file_name = '外卖评论.csv'
+data = []
 
 def read_csv(input_file_name):
     """
     读取外卖评论.csv的数据
     """
-    with open(input_file_name,'r',encoding='utf-8') as csv_file:
+    with open(input_file_name,'r') as csv_file:
        csv_reader = csv.reader(csv_file)
+       for line in csv_reader:
+            seg_list = jieba.lcut(line)
+            data.append(seg_list)
        csv_file.close()
 
-def save_csv(target_list, output_file_name):
+read_csv(input_file_name)
+output_file_name = open('csv_test.csv','w')
+
+def save_csv(data, output_file_name):
     """
     将数据写入csv文件
     """
+    writer = csv.writer(output_file_name)
+    writer.writerows(data)
+    output_file_name.close()
 
+save_csv(data, output_file_name)
