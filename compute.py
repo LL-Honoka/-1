c7f0e0c32f6a75a25db00afc84a1772f366811b0
Ubuntu <1023064692@qq.com>
2020-11-02 20:49:35 +0800

    Social-Computing-Project-One

--- compute.py
+++ compute.py
@@ -1,31 +1,42 @@
 import jieba
-
+import jieba.analyse
+from pmi import *
+import re
 input_file_name = '外卖评论.csv'
-data = []
+documents = []
 
 def read_csv(input_file_name):
     """
-    读取外卖评论.csv的数据
+    读取外卖评论.csv的数据,并计算pmi
     """
-    with open(input_file_name) as csv_file:
-       for line in csv_file.readlines():
-            seg_list = jieba.cut(line)
-            data.append("/".join(seg_list))
-       csv_file.close()
+    f = open(input_file_name, 'r')
+    data = f.readlines()
+    if data is not None:
+        for sentence in data:
+            extractwords = []
+            re.sub('\[.*?\]', '', sentence) #去掉标点符号
+            words = jieba.cut(sentence)
+            wordlist = []
+            for word in words:
+                wordlist.append(word)
+            sentence = ','.join(wordlist)
+            words = jieba.analyse.extract_tags(sentence, 5)
+            for word in words:
+                extractwords.append(word)
+            documents.append(set(extractwords))
+    pm = PMI(documents)
+    pmi = pm.get_pmi()
+    return pmi
+    f.close()
+pmi_document = read_csv(input_file_name)
+pmi_document.items()
+pmi_list = list(pmi_document.items())
+pmi_list.sort(key=lambda x:x[1],reverse=True)
+print("最具有正向情感的50词:")
+for i in range(50):
+    print(str(pmi_list[i]))
+print("最具有负向情感的词:")
+for i in range(50):
+    print(str(pmi_list[len(pmi_list) - i - 1]))
 
-read_csv(input_file_name)
-output_file_name = open('csv_test.csv','w')
 
-def compute(data):
-    """
-    算出两个词语的共现概率、单独出现的概率，并套入公式
-    """
-    
-def save_csv(data, output_file_name):
-    """
-    将数据写入csv文件
-    """
-    output_file_name.writelines(data)
-    output_file_name.close()
-
-save_csv(data, output_file_name)
